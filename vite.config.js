d53b171a200c787c7e6911f3f3bdcbf6d3ea9a1d
simon-ami <sbenedict@gmx.de>
2026-01-06 15:37:25 +0100

    add gh action workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/map_kepler/',
   plugins: [react()],
   define: {
     // Node.js globals für Browser ersetzen
